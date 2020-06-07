065d8665c85509cd5adc1621e6553e82c7153fbb
ArihanthMutha <62700226+ArihanthMutha@users.noreply.github.com>
2020-06-07 15:52:00 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+        this.body=Constraint.create(options)
+        World.add(world,this.body)
+    }
+    display(){
+        strokeWeight(7)
+        line(this.body.bodyA.position.x,this.body.bodyA.position.y,this.body.bodyB.position.x,this.body.bodyB.position.y)
+    }
+}
